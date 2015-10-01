748ff1014eb28c044ea9e34d5bf26740aeec0d27
btcstealth <stealthy_stealth@hotmail.com>
2015-10-01 16:24:42 +0200

    new

--- PP/exercises/4/exercise 4.12.rkt
+++ PP/exercises/4/exercise 4.12.rkt
@@ -1,9 +1,24 @@
 #lang racket
 
 
+(define (p-cps a b k0)
+  (plus a b (lambda(v1)
+              ; given the sum of and b in v1, now compute a-b and
+              ; the product. 
+              (sub a b (lambda(v2)
+                         ; given the sum in v1 and the difference in v2, now 
+                         ; carry out the rest of the computation (the multiplication)
+                         ; and pass the result to k0.
+                         (mult v1 v2 k0))))))
 
-(define (discriminant a b c)
-  (sub (square b) (mult (mult 4 a) c)))
+
+
+(define (discriminant a b c k0)
+  (square b (lambda(v1) v1)
+          (mult )
+          ))
+
+  ;(sub (square b) (mult (mult 4 a) c)))
 
 
 ; AUX functions:
